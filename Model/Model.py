bdc831a1f1019139d33f3b4b9a90d493a0a46e9f
xyavid <xyun3525@gmail.com>
2026-09-12 18:40:11 +0800

    feat：实现FFN

--- Model/Model.py
+++ Model/Model.py
@@ -78,6 +78,7 @@ import torch.nn as nn
 import math
 from typing import Optional,Tuple
 from torch.nn import functional as F
+from transformers.activations import ACT2FN
 
 #继承nn.Module类
 class RMSNorm(nn.Module):
@@ -264,3 +265,26 @@ class Attention(nn.Module):
         #进行残差架构下的dropout也即对x+f(x)里的f(x)
         output=self.resid_dropout(self.o_proj(output))
         return output,past_kv
+
+class FeedForward(nn.Module):
+    #初始化
+    #升维
+    #降维
+    #门控
+    #dropout
+    #激活函数
+    def __init__(self, args:MokioMindConfig):
+        super().__init__()
+        if args.intermediate_size is None:
+            intermediate_size=int(args.hidden_size*8/3)
+            args.intermediate_size=64*((intermediate_size+64-1)//64)
+
+        self.up_proj=nn.Linear(args.hidden_size,args.intermediate_size,bias=False)#升
+        self.down_proj=nn.Linear(args.intermediate_size,args.hidden_size,bias=False)#降
+        self.gate_proj=nn.Linear(args.hidden_size,args.intermediate_size,bias=False)#门控
+        self.dropout=nn.Dropout(args.dropout)
+        self.act_fn=ACT2FN[args.hidden_act]#激活函数
+
+    def forward(self,x):
+        #完整实现ffn链路
+        return self.dropout(self.down_proj(self.act_fn(self.up_proj(x))*self.gate_proj(x)))
