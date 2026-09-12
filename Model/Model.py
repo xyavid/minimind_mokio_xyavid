e387a7d3c4c2178d84c5b4e27dbe82c18c02aad4
xyavid <xyun3525@gmail.com>
2026-09-12 20:50:12 +0800

    feat：block搭建

--- Model/Model.py
+++ Model/Model.py
@@ -288,3 +288,33 @@ class FeedForward(nn.Module):
     def forward(self,x):
         #完整实现ffn链路
         return self.dropout(self.down_proj(self.act_fn(self.up_proj(x))*self.gate_proj(x)))
+
+class MokioMindBlock(nn.Module):
+    def __init__(self, layer_id:int,config:MokioMindConfig) -> None:
+        super().__init__()
+        #初始化参数
+        self.num_attention_heads=config.num_attention_heads#头数
+        self.hidden_size=config.hidden_size#维度
+        self.head_dim=self.hidden_size//self.num_attention_heads#单头维度
+        self.self_attn=Attention(config)
+
+        self.layer_id=layer_id
+        self.input_layernorm=RMSNorm(config.hidden_size,eps=config.rms_norm_eps)
+        self.post_attention_layernorm=RMSNorm(config.hidden_size,eps=config.rms_norm_eps)
+        self.mlp=FeedForward(config)
+
+    def forward(self,hidden_states,positon_embeddings,past_key_value=None,use_cache=False,attention_mask=None):
+        residual=hidden_states
+        #GQA attention
+        hidden_states,present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states),
+            positon_embeddings,
+            past_key_value,
+            use_cache,
+            attention_mask
+        )
+        #残差处理
+        hidden_states = residual + hidden_states
+        #FFN
+        hidden_states=hidden_states+self.mlp(self.post_attention_layernorm(hidden_states))
+        return hidden_states,present_key_value
